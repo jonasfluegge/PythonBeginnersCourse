49259a2fe6cd4418c05ca0ebe8fab7a0cb5f9d63
Flügge <Jonas.Fluegge@waelzholz.com>
2022-05-12 21:09:05 +0200

    Initial commit

---
+++ Week 1/Unit_3.py
@@ -0,0 +1,6 @@
+name = input("Please enter a name: ")
+start = input("Please enter a start: ")
+destination = input("Please enter a destination: ")
+mean = input("Please enter a means of transportation: ")
+
+print(name, "wants to travel from", start, "to", destination, "by", mean)
